f2f4ff6f46fe5c448bb2d42fe54acdad6eeb55ca
Tim Wilson <twilsonco@gmail.com>
2023-07-21 15:24:42 -0600

    add nnffLog and startup alert

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -175,10 +175,12 @@ class Controls:
     self.experimental_mode = False
     self.v_cruise_helper = VCruiseHelper(self.CP)
     self.recalibrating_seen = False
+    self.nnff_alert_shown = False
 
     # TODO: no longer necessary, aside from process replay
     self.sm['liveParameters'].valid = True
     self.can_log_mono_time = 0
+    
 
     self.startup_event = get_startup_event(car_recognized, controller_available, len(self.CP.carFw) > 0)
 
@@ -228,6 +230,14 @@ class Controls:
     # no more events while in dashcam mode
     if self.read_only:
       return
+    
+    # show alert to indicate whether NNFF is loaded
+    if not self.nnff_alert_shown and self.sm.frame % 1000 == 0 and self.CP.lateralTuning.which() == 'torque':
+      self.nnff_alert_shown = True
+      if self.LaC.use_nn:
+        self.events.add(EventName.torqueNNFFLoadSuccess)
+      else: 
+        self.events.add(EventName.torqueNNFFNotLoaded)
 
     # Block resume if cruise never previously enabled
     resume_pressed = any(be.type in (ButtonType.accelCruise, ButtonType.resumeCruise) for be in CS.buttonEvents)
